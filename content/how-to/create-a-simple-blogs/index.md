304567fe826bd0d30bde14b951943a44c7cdbbd1
WeiJyunYe <weijyunye@gmail.com>
2023-08-01 20:53:29 +0800

    rebuild notes and blog

---
+++ content/how-to/create-a-simple-blogs/index.md
@@ -0,0 +1,10 @@
+---
+title: 如何用Gatsby架設一個簡易的部落格
+date: "2023-08-01"
+category: Coding
+tags:
+  - how-to
+  - frontend
+---
+
+施工中
---
+++ src/components/layout.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { Link } from "gatsby";
+
+const Layout = () => {
+  return <div>layout</div>;
+};
+
+export default Layout;
